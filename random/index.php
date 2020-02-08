ba187d9d1ba4bd061c3f6c5ae713513cd501f31a
root <root@geulah.org.il>
2020-02-08 09:45:27 +0200

    update

--- random/index.php
+++ random/index.php
@@ -1,116 +1,289 @@
-<?php
-if ( isset( $argv ) ) {
-  parse_str(
-	    join( "&", array_slice( $argv, 1 )
-		  ), $_GET );
-}
-
-if (!empty($_GET['random'])) {$random=$_GET['random'];} else {$random=-1;}
-
-$dq ='"';
-include 'random_pasuk.php';
-
-$url= $dq . $home_url . '?' . $shortbook . $chap . ':' . $verse . '-' . $verse . $dq;
-
-?>
-<html>
-<title><?php echo $shortbook . $chap . ':' . $verse . "/" . $text_output_heb; ?></title>
-
+<html dir="rtl" lang="he">
 <head>
   <link rel="stylesheet" href="EzraSIL-webfont.css" type="text/css">
   <meta content="text/html; charset=UTF-8" http-equiv="content-type">
 </head>
-
-<style>
-#nav
-{
-background-color: red;
-float: left;
-width: 200px;
-position:relative;
-font-size: xx-large;
-}
-
-
-#container
-{
-width:1000px;
-position:relative;
-}
-
-#detail{
-  //For center align
-  margin-left: auto;
-  margin-right: auto;
-  // For grow & shrink as per screen size change values as per your requirements
-  width: 80%;
-  height: 50%;
- font-size: large;
-}
-</style>
-
-<hr>
 <body>
-  
-<?php /* echo '<a href=' . $dq . $home_url . '?' . $shortbook . $chap . ':' . $verse . '-' . $verse . $dq . 'target="_blank">' . $shortbook . $chap . ':' . $verse . "/" . $text_output_heb . '</a>'; 
-
-
- if(empty($pol))
- { $pol="manual";
- }
- echo  'random number value ' . $random . ' date:' . $pol . '<br>';
-  */
-?>
-
-<div id="container">
-<div id="nav">
-    <ul><?=$he_url_samepage; ?>Pasuk</a> </ul>
-    <ul><?=$he_url_samepage_full_chapter; ?>Perek</a> </ul>
-    <ul><?="<a href='".$whereami."'>New חדש</a>"?> </ul>
-</div>
-<div id="detail">
-
-        <?= '<a target = "_blank"  href=' . $dq . 'http://geulah.org.il/random/?random=' . $random . $dq . '>'; ?>
-	<image style="float:right;margin:5px;" title="בעברית" src="../files/il.png" alt="Hebrew" width="50px" height="50px"/></a>
-        <?= $en_url; ?>
-	<image style="float:right;margin:5px;" title="English" src="../files/en.png" alt="English" width="50px" height="50px"/></a>
-	<?= $fr_url; ?>
-	<image style="float:right;margin:5px;" title="français" src="../files/fr.png" alt="French"" width="50px" height="50px"/></a>
-	<?= $ru_url; ?>
-	<image style="float:right;margin:5px;" title="русски" src="../files/ru.png" alt="Russian"  width="50px" height="50px"/></a>
-    <a target = '_blank' href="form1.php">	
-	<image style="float:right;margin:5px;" title="multi" src="../files/earth180.png" alt="Multi"  width="50px" height="50px"/></a>
-
-        <h3><?= $text_output_heb ?></h3>
-        <p><?= $text_output ?></p>
-    <hr>
-</div>
-</div>
-
-
-<object data= <?='"'. $home_url . '?' . $shortbook . $chap . ':' . $verse . '-' . $verse . '"' ?>
- width="1000" height="500">
-<embed src=  <?='"'. $home_url . '?' . $shortbook . $chap . ':' . $verse . '-' . $verse . '"' ?> width="1000" height="500"> </embed>
- Error: Embedded data could not be displayed. 
-</object>
-
+<h2>
 <?php
-$cookie_name = "language";
 
-if(!isset($_COOKIE[$cookie_name]))
-	{
-  #echo "Cookie named '" . $cookie_name . "' is not set! <br>";
-} else {
-	$version = $_COOKIE[$cookie_name];
-    echo "<object data=" . '"' . $home_eng_url . '?' . $shortbook . $chap . ':' . $verse . '-' . $verse . '&version=' . $version . '"' . 'width="1000" height="500">';
-    echo "<embed src=" . '"' . $home_eng_url . '?' . $shortbook . $chap . ':' . $verse . '-' . $verse . '&version=' . $version . '"' . 'width="1000" height="500"> </embed>';
-    echo " Error: Embedded data could not be displayed."; 
-    echo "</object>";
-  # echo "Cookie named '" . $cookie_name . "' is " . $name . "<br>";
+	// seed with microseconds
+	function make_seed(){
+		list($usec, $sec) = explode(' ', microtime());
+		return (float) $sec + ((float) $usec * 100000);
+	}
+
+	srand(make_seed());
+	$randval = rand();
+// check the entire tanach
+//for ($randval = 0; $randval <= 23212; $randval++) {
+//    echo "The number is: $random <br>";
+ 
+	$random = ($randval%23212)+1;
+	
+
+//$random=1..23213;
+
+
+// print("</br><h5>Random value: " . $random ."</h5>");
+
+$file = 'letteris_utf8.txt';
+
+$word = $random . "0";
+$searchfor = "\t" . $word . "\t";
+
+// the following line prevents the browser from parsing this as HTML.
+//header('Content-Type: text/plain');
+
+// get the file contents, assuming the file to be readable (and exist)
+$contents = file_get_contents($file);
+// escape special characters in the query
+$pattern = preg_quote($searchfor, '/');
+// finalise the regular expression, matching the whole line
+$pattern = "/^.*$pattern.*\$/m";
+// search, and store all matching occurences in $matches
+if(preg_match_all($pattern, $contents, $matches)){
+//   echo "Found matches:\n";
+$result = implode("\n",$matches[0]);
+list($bk,$ch,$vr,$ax,$value,$verse) = explode("\t",$result);
+
+$book = rtrim($bk,"O");
+switch ($book) {
+	case "30":
+		$fullbook="Amos";
+		$shortbook="Am";
+		$hebbook="עמוס";
+		break;
+	case "27":
+		$fullbook="Daniel";
+		$shortbook="Dan";
+		$hebbook="דניאל";
+		break;
+	case "05":
+		$fullbook="Deuteronomy";
+		$shortbook="Deut";
+		$hebbook="דברים";
+		break;
+	case "17":
+		$fullbook="Esther";
+		$shortbook="Esth";
+		$hebbook="אסתר";
+		break;
+	case "02":
+		$fullbook="Exodus";
+		$shortbook="Ex";
+		$hebbook="שמות";
+		break;
+	case "26":
+		$fullbook="Ezekiel";
+		$shortbook="Ezek";
+		$hebbook="יחזקאל";
+		break;
+	case "15":
+		$fullbook="Ezra";
+		$shortbook="Ezra";
+		$hebbook="עזרא";
+		break;
+	case "01":
+		$fullbook="Genesis";
+		$shortbook="Gen";
+		$hebbook="בראשית";
+		break;
+	case "35":
+		$fullbook="Habakkuk";
+		$shortbook="Hab";
+		$hebbook="חבקוק";
+		break;
+	case "37":
+		$fullbook="Haggai";
+		$shortbook="Hag";
+		$hebbook="חגי";
+		break;
+	case "28":
+		$fullbook="Hosea";
+		$shortbook="Hos";
+		$hebbook="הושע";
+		break;
+	case "13":
+		$fullbook="1 Chronicles";
+		$shortbook="1Chr";
+		$hebbook="דברי הימים א'";
+		break;
+	case "11":
+		$fullbook="1 Kings";
+		$shortbook="1Kings";
+		$hebbook="מלכים א'";
+		break;
+	case "09":
+		$fullbook="1 Samuel";
+		$shortbook="1Sam";
+		$hebbook="שמואל א'";
+		break;
+	case "14":
+		$fullbook="2 Chronicles";
+		$shortbook="2Chr";
+		$hebbook="דברי הימים ב'";
+		break;
+	case "12":
+		$fullbook="2 Kings";
+		$shortbook="2Kings";
+		$hebbook="מלכים ב'";
+		break;
+	case "10":
+		$fullbook="2 Samuel";
+		$shortbook="2Sam";
+		$hebbook="שמואל ב'";
+		break;
+	case "23":
+		$fullbook="Isaiah";
+		$shortbook="Isa";
+		$hebbook="ישעיה";
+		break;
+	case "24":
+		$fullbook="Jeremiah";
+		$shortbook="Jer";
+		$hebbook="ירמיה";
+		break;
+	case "18":
+		$fullbook="Job";
+		$shortbook="Job";
+		$hebbook="איוב";
+		break;
+	case "29":
+		$fullbook="Joel";
+		$shortbook="Joel";
+		$hebbook="יואל";
+		break;
+	case "32":
+		$fullbook="Jonah";
+		$shortbook="Jon";
+		$hebbook="יונה";
+		break;
+	case "06":
+		$fullbook="Joshua";
+		$shortbook="Josh";
+		$hebbook="יהושע";
+		break;
+	case "07":
+		$fullbook="Judges";
+		$shortbook="Judg";
+		$hebbook="שופטים";
+		break;
+	case "21":
+		$fullbook="Ecclesiastes";
+		$shortbook="Eccl";
+		$hebbook="קהלת";
+		break;
+	case "25":
+		$fullbook="Lamentations";
+		$shortbook="Lam";
+		$hebbook="איכה";
+		break;
+	case "03":
+		$fullbook="Leviticus";
+		$shortbook="Lev";
+		$hebbook="ויקרא";
+		break;
+	case "39":
+		$fullbook="Malachi";
+		$shortbook="Mal";
+		$hebbook="מלאכי";
+		break;
+	case "33":
+		$fullbook="Micah";
+		$shortbook="Mic";
+		$hebbook="מיכה";
+		break;
+	case "34":
+		$fullbook="Nahum";
+		$shortbook="Nah";
+		$hebbook="נחום";
+		break;
+	case "16":
+		$fullbook="Nehemiah";
+		$shortbook="Neh";
+		$hebbook="נחמיה";
+		break;
+	case "04":
+		$fullbook="Numbers";
+		$shortbook="Num";
+		$hebbook="במדבר";
+		break;
+	case "31":
+		$fullbook="Obadiah";
+		$shortbook="Ob";
+		$hebbook="עובדיה";
+		break;
+	case "20":
+		$fullbook="Proverbs";
+		$shortbook="Prov";
+		$hebbook="משלי";
+		break;
+	case "19":
+		$fullbook="Psalms";
+		$shortbook="Ps";
+		$hebbook="תהילים";
+		break;
+	case "08":
+		$fullbook="Ruth";
+		$shortbook="Ruth";
+		$hebbook="רות";
+		break;
+	case "22":
+		$fullbook="Song of songs";
+		$shortbook="Song";
+		$hebbook="שיר השירים";
+		break;
+	case "38":
+		$fullbook="Zechariah";
+		$shortbook="Zech";
+		$hebbook="זכריה";
+		break;
+	case "36":
+		$fullbook="Zephaniah";
+		$shortbook="Zeph";
+		$hebbook="צפניה";
+		break;
+ }
+
+   $b = " ספר:";
+   $c = " פרק:";
+   $v = " פסוק:";
+   $white = array("\t","\n","\r","\0","\x0B");
+   $numbers = " O0123456789";
+   $pasuk = str_replace ($white, " ", $verse);
+   $pasuk = trim ($pasuk, $numbers);
+
+   echo "</br><h2>" . $pasuk . "</h2></br>";
+   $home_url="http://geulah.org.il/Tanach/Tanach.xml";
+   $url_output=$home_url . '?' . $shortbook . $ch . ':' . $vr . '-' . $vr;
+   $output_1='<a href="';
+   $output_2='"/>';
+   $output_3="</a>";
+
+//   $text_output =$fullbook . ' ' . $ch . ' ' . $vr;
+   $text_output=  "\n" . $b . $hebbook  . " " . $c . $ch . " " . $v . $vr . "<br>\n";
+   echo($output_1 . $url_output . $output_2 . $text_output . $output_3);
+
+   $url_output=$home_url . '?' . $shortbook . $ch . '-' . $ch;
+   $text_output=  "\n" . $b . $hebbook  . " " . $c . $ch . "<br>\n";
+
+   echo($output_1 . $url_output . $output_2 . $text_output . $output_3);
+   
 }
-echo  date("d-m-Y H:i:s");
-echo "<div style='visibility: hidden'>The number is: $random </div><br>";
+else{
+         echo "No matches found";
+ }
+echo "<details><date>" . date("d-m-Y H:i:s") . "</date>";
+// grep "3900" letteris_utf8.txt |grep 01O |grep 8|hexdump -C|grep af
+//Comments_utf8.txt
+// if $pasuk contains "af" then find pasuk's comment from Comments_utf8.txt 
+//echo "<div style='visibility: hidden'>
+echo "<p> The number is:  $random </p>";
+echo "</details>"
 
+//}
 ?>
-</body></html>
+</body>
+</html>
 
