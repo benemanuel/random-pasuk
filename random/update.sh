53f3aaeaad5d142d77c27c36b3bcad7d9cf0d2b8
Avi BenEmanuel <benemanuel@users.noreply.github.com>
2021-08-21 17:28:27 +0300

    change in file format from txt to utf8_txt

--- random/update.sh
+++ random/update.sh
@@ -1,2 +1,2 @@
-wget -O letteris_utf8.txt  https://raw.githubusercontent.com/benemanuel/letteris/master/letteris_utf8.txt
-wget -O Comments_utf8.txt https://raw.githubusercontent.com/benemanuel/letteris/master/Comments_utf8.txt
+wget -O letteris_utf8.txt  https://raw.githubusercontent.com/benemanuel/letteris/master/letteris_utf8.utf8_txt
+wget -O Comments_utf8.txt https://raw.githubusercontent.com/benemanuel/letteris/master/Comments_utf8.utf8_txt
