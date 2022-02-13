1718da06bc8e899aee8bdac50b6c602b38028219
Avi BenEmanuel <benemanuel@geulah.org.il>
2022-02-13 20:59:22 +0200

    update

--- random/check/index.php
+++ random/check/index.php
@@ -292,8 +292,10 @@ echo '<p dir="rtl" lang="he">';
 
 $comment_row=exec("awk '$4 == $key' $comment");
 $verse_row=exec("awk '$4 == $key' $file");
+$editor_row=exec("awk '$4 == $key'Editornotes_utf8.txt");
 echo "</p><p> The Verse is: $verse_row</p>";
 echo "<p> The Comments are: $comment_row</p>";
+echo "<p> The Editor's Comments are: $editor_row</p>";
 echo "</details>"
 
 //}
